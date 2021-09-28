b4ee9f4b430a6b74ebc938b6e30da1c111b2af41
Henrique <henriquefernandes.gouveia@gmail.com>
2021-09-28 10:03:20 -0300

    aula-pratica-js

---
+++ Atividade 01/ex01.js
@@ -0,0 +1,16 @@
+//parte 1
+var dominio1 = "mundojs";
+var dominio2 = "google";
+console.log(`Olá, ${dominio1}! O site www.${dominio2}.com te ajuda a achar muitos matérias de estudo.`);
+
+//parte 2
+var valorA = "casa";
+var valorB = "asa";
+var valorA = valorA.toUpperCase();
+var valorB = valorB.toLowerCase();
+console.log(valorA, valorB);
+
+var comparacao = valorA.localeCompare(valorB);
+console.log(comparacao)
+
+
---
+++ Atividade 02/ex02.js
@@ -0,0 +1,13 @@
+var tabuada = [5,10,15,20,25,30,35,40,45,50];
+console.log(tabuada.join(" + 5 = "));
+
+//parte 2
+var temperatura = [54,78,56,98,0,12,11,37];
+console.log((temperatura[0] - 32)/ 1.8);
+console.log((temperatura[1] - 32)/ 1.8);
+console.log((temperatura[2] - 32)/ 1.8);
+console.log((temperatura[3] - 32)/ 1.8);
+console.log((temperatura[4] - 32)/ 1.8);
+console.log((temperatura[5] - 32)/ 1.8);
+console.log((temperatura[6] - 32)/ 1.8);
+console.log((temperatura[7] - 32)/ 1.8);
